6edae2a9ad2c353fd6fc82c3914c8a704a35e187
KDSGYT <kdsgyt@gmail.com>
2020-07-17 08:59:47 -0400

    Now you can response to custom routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 const data = require('./data') ;
+const counts = require('./data/counts.json')
 
 
 
@@ -11,10 +12,16 @@ app.get('/', (req, res) => {
     res.end();
 })
 
-app.get('/data', (req,res ) =>{
-    res.json(data);
-    res.end();
+app.get('/:keyword', (req,res ) =>{
+    console.log(req.params);
+    res.status(200).type('json').json(data[req.params.keyword]);
 })
+
+app.get('/channel/:id', (req, res) => {
+    console.log(req.params);
+    res.status(200).type('json').json(counts[req.params.id]);
+})
+
 
 app.listen( port, () =>{
     console.log(`listening on port ${port}.....`);
